10727cb68d35d333cffd52e8585112a256004d5b
InsonusK <insonus.k@gmail.com>
2020-08-18 18:12:50 +0300

    add error auto rising

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const core = require('@actions/core');
 try {
   const a = core.getInput('a');
   const b = core.getInput('b');
-  core.setOutput("result", a === b);
+  const error_result = core.getInput('b');
+  const error_message = core.getInput('b');
+
+  let result = getResult(a, b);
+  if (result === error_result){
+    core.setFailed(error_message);
+  }
+
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function getResult(a, b){
+  let return_result = a === b ? "true" : "false";
+  core.setOutput("result", return_result);
+  return return_result;
 }
